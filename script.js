4a66c6e35e7b31d1d5bcd99524110fc29bef40dd
Aashish Shilkande <83578134+Aashish257@users.noreply.github.com>
2025-07-22 21:14:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species: string) {
+		this.species = species;
+	}
 
-class Dog extends Animal {}
+	get species() {
+		return this.species;
+	}
 
-class Cat extends Animal {}
+	makeSound() {
+		console.log(`The ${this.species} make a sound`);
+	}
+}
+
+class Dog extends Animal {
+	constructor(species: string) {
+		super(species: string);
+	}
+
+	bark() {
+		console.log(`woof`);
+	}
+}
+
+class Cat extends Animal {
+	constructor(species: string) {
+		super(species: string);
+	}
+
+	purr() {
+		console.log(`purr`);
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
